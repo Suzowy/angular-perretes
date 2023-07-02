259160d000e2003478b6fec7f4afa36216eb2ede
Suzowy <suzowy@gmail.com>
2023-07-02 12:59:06 +0200

    vercel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,13 +8,21 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   username: string;
   password: string;
+  loggedInUser: string;
 
   constructor() {
     this.username = '';
     this.password = '';
+    this.loggedInUser = '';
   }
+
   login() {
     console.log('Username:', this.username);
     console.log('Password:', this.password);
+
+    // Aquí puedes realizar la lógica de autenticación
+    // y asignar el nombre de usuario a la propiedad loggedInUser después de iniciar sesión.
+    // Por ahora, simplemente asignaremos el nombre de usuario ingresado.
+    this.loggedInUser = this.username;
   }
 }
